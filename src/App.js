f96ed1d20523b9da271153b8e5b6b597a44056a9
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-05-24 21:31:06 -0400

    Update Readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
+
+  fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => {});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePage/>
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import useState from "react"
+
+function HomePage() {
+    
+    return (
+        <section className="home-page">
+            <h1>YouTube</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </section>
+    );
+}
+  
+export default HomePage;
