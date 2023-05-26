6a67f27878dc4451be818ad11efb4d2aef2660d6
RK-404 <rizwan_k@RIZWANs-MacBook-Air.local>
2023-05-26 01:41:22 -0400

    Added About page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import './App.css';
-import Header from './components/Header';
+import React from "react";
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
+import About from './components/About';
 
 function App() {
 
-  fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${process.env.REACT_APP_API_KEY}`)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => {});
+  // const apiKey = process.env.REACT_APP_API_KEY;
+
+  // fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}`)
+  // .then(response => response.json())
+  // .then(response => console.log(response))
+  // .catch(err => {});
+
+  //https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${searchKey}&type=video&key=${apiKey}
 
   return (
     <div className="App">
-      <Header/>
+      <NavBar/>
+      <HomePage/>
+      <About/>
     </div>
   );
 }
