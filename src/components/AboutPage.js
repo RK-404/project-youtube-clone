d71e2ec853e0a9529b129fc16875147b0009b609
Judy <judychue@pursuit.org>
2023-05-28 18:17:18 -0400

    added link to about page and created AboutPage component

---
+++ src/components/AboutPage.js
@@ -0,0 +1,10 @@
+function AboutPage() {
+    return (
+        <div>
+          <h1>Hello world</h1>
+          {/* <input id="icon_prefix" type="text" class="validate"/> */}
+        </div>
+    )
+  }
+
+  export default AboutPage;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,58 @@
-// import React from "react"
+// import { Link } from "react-router-dom";
 
+// function NavBar() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+//       <div className="container-fluid">
+//         <Link to="className="navbar-brand" href="/" style={{ color: "white" }}>
+//           Youtube
+//         </Link>
+//         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+//           <div className="navbar-nav">
+//             <Link to="nav-link active"
+//               aria-current="page"
+//               href="/"
+//               style={{ color: "white" }}
+//             >
+//               Home
+//             </Link>
+//             <Link to="nav-link" href="/about" style={{ color: "white" }}>
+//               About
+//             </Link>
+//           </div>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default NavBar;
 
 function NavBar() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-danger">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/" style={{ color: "white" }}>
-            Youtube
-          </a>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a
-                className="nav-link active"
-                aria-current="page"
-                href="/"
-                style={{ color: "white" }}
-              >
-                Home
-              </a>
-              <a className="nav-link" href="/about" style={{ color: "white" }}>
-                About
-              </a>
-            </div>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/" style={{ color: "white" }}>
+          Youtube
+        </a>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a
+              className="nav-link active"
+              aria-current="page"
+              href="/"
+              style={{ color: "white" }}
+            >
+              Home
+            </a>
+            <a className="nav-link" href="/about" style={{ color: "white" }}>
+              About
+            </a>
           </div>
         </div>
-      </nav>
-    );
-  }
-  
-  export default NavBar;
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
