b2703e01a1c912473fb565dd75e25ed60b8850f0
tshering gurung <tshering@tsherings-air.myfiosgateway.com>
2023-06-02 21:31:45 -0400

    comment section

--- src/components/VideoCard/Form.jsx
+++ src/components/VideoCard/Form.jsx
@@ -1,20 +1,35 @@
+import { useState } from 'react';
 import './form.css'
 
+
 function Form() {
+    const [input, setInput] = useState([])
+
     function handleSubmit(e) {
         e.preventDefault()
         console.log(e.target.name.value);
         console.log(e.target.comment.value);
+        setInput([...input, {name: e.target.name.value, comment: e.target.comment.value}])
+        e.target.name.value = ""
+        e.target.comment.value = ""
     }
 
 
     return (
         <form className='form' onSubmit={handleSubmit}>
-            <label htmlFor="">Name</label>
-            <input type="text" name='name' placeholder='Name...' />
-            <label htmlFor="">Comment</label>
+            <label htmlFor="name">Name</label>
+            <input type="text" name='name' placeholder='Name...' required/>
+            <label htmlFor="comment">Comment</label>
             <input type="text" name='comment' placeholder='...' />
             <button>Submit</button>
+            <hr />
+            {input.map(el => 
+            <div className='comment-box'>
+                <h4>{el.name}</h4> 
+                <p>{el.comment}</p>
+                <hr />
+            </div>
+            )}
           </form>
     )
 }
