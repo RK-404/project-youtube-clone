d664801955ca3fd4e6b06d1e95426b46027c2cd4
Lakisha J <lakishajohnson@pursuit.org>
2023-06-05 19:03:00 -0400

    Updated files for 400 error

--- src/App.js
+++ src/App.js
@@ -13,30 +13,29 @@ function App() {
   const [search, setSearch] = useState("");
   const apiKey = process.env.REACT_APP_API_KEY;
   const [showModal, setShowModal] = useState(false);
-  const [errorStatus, setErrorStatus] = useState(null);
-
-  // useEffect(() => {
-  //   fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&type=video&maxResults=20`)
-  //   .then((response) => response.json())
-  //   .then((response) => setAllVideo(response.items))
-  //   .catch((error) => console.log(error))
-  // },[]);
 
   function handleInput(event) {
     setSearch(event.target.value);
   }
 
-  function handleSearch(event) {
-    // event.preventDefault();
+  function handleSearch() {
     if (search) {
       fetch(
         `https://www.googleapis.com/youtube/v3/search?q=${search}&key=${apiKey}&part=snippet&type=video&maxResults=20`
       )
-        .then((response) => response.json())
-        .then((response) => setAllVideo(response.items))
-        .catch((error) => console.log(error));
-      setShowModal(false);
-      setErrorStatus(400);
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("API request failed");
+          }
+          return response.json();
+        })
+        .then((response) => {
+          setAllVideo(response.items);
+        })
+        .catch((error) => {
+          console.log(error);
+          setShowModal(true);
+        });
     }
     setSearch("");
   }
@@ -47,15 +46,14 @@ function App() {
     }
   }
 
+  function resetVideos() {
+    setAllVideo([]);
+  }
+
   return (
     <div>
       <Router>
-        <NavBar />
-        {/* <Modal
-          setShowModal={setShowModal}
-          errorStatus={errorStatus}
-          showModal={showModal}
-        /> */}
+        <NavBar resetVideos={resetVideos} />
         <Routes>
           <Route
             path="/"
@@ -66,26 +64,17 @@ function App() {
                 handleEnter={handleEnter}
                 userInput={search}
                 videos={allVideo}
-              />
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <Modal
-                errorStatus={errorStatus}
                 showModal={showModal}
-                setShowModal={setShowModal}
               />
             }
           />
+
+          {/* <Route path="*" element={<Modal setShowModal={setShowModal} />} /> */}
           <Route path="/videos/:id" element={<Video allVideo={allVideo} />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
-        {/* {errorStatus && <Modal />} */}
-        {/* {errorStatus === 400 && <Modal />}
-         */}
       </Router>
+      {showModal && <Modal setShowModal={setShowModal} />}
     </div>
   );
 }
